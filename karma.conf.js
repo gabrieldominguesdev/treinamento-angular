7efa3fac03f1b41df4356fb29cdbd62388c15f8a
Gabriel Domingues <gabriell.fad@gmail.com>
2022-03-18 19:32:43 -0300

    ajustes de nomenclatura

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/bma-treinamento'),
+      dir: require('path').join(__dirname, './coverage/angular-treinamento'),
       subdir: '.',
       reporters: [
         { type: 'html' },
